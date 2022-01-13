f96b55632e4c04bd157fe6e7efc28ea37c71c83d
Mattias Egly <48134992+mattiasegly@users.noreply.github.com>
2022-01-13 14:19:27 +0100

    Build script updates

--- build_as_latest.sh
+++ build_as_latest.sh
@@ -3,9 +3,16 @@
 DOCKER_REPO=mattiasegly/base-image
 SOURCE_BRANCH=buster
 
+#Pull base images
 docker pull amd64/debian:$SOURCE_BRANCH
 docker pull i386/debian:$SOURCE_BRANCH
 docker pull balenalib/rpi-raspbian:$SOURCE_BRANCH
+
+#Start fresh
+for ARCH in amd64 386 arm
+do
+docker image rm $DOCKER_REPO:$SOURCE_BRANCH-$ARCH
+done
 
 #Build and push
 for ARCH in amd64 386 arm
@@ -13,6 +20,23 @@ do
 docker build -f Dockerfile.$ARCH -t $DOCKER_REPO:$SOURCE_BRANCH-$ARCH .
 docker push $DOCKER_REPO:$SOURCE_BRANCH-$ARCH
 done
+
+#Release specific tag
+docker manifest create \
+	$DOCKER_REPO:$SOURCE_BRANCH \
+	$DOCKER_REPO:$SOURCE_BRANCH-amd64 \
+	$DOCKER_REPO:$SOURCE_BRANCH-386 \
+	$DOCKER_REPO:$SOURCE_BRANCH-arm
+
+for ARCH in amd64 386 arm
+do
+docker manifest annotate \
+	$DOCKER_REPO:$SOURCE_BRANCH \
+	$DOCKER_REPO:$SOURCE_BRANCH-$ARCH --os linux --arch $ARCH
+done
+
+docker manifest push --purge \
+	$DOCKER_REPO:$SOURCE_BRANCH
 
 #Default "latest" tag
 docker manifest create \
@@ -33,20 +57,3 @@ docker manifest push --purge \
 
 docker manifest inspect \
 	$DOCKER_REPO
-
-#Release specific tag
-docker manifest create \
-	$DOCKER_REPO:$SOURCE_BRANCH \
-	$DOCKER_REPO:$SOURCE_BRANCH-amd64 \
-	$DOCKER_REPO:$SOURCE_BRANCH-386 \
-	$DOCKER_REPO:$SOURCE_BRANCH-arm
-
-for ARCH in amd64 386 arm
-do
-docker manifest annotate \
-	$DOCKER_REPO:$SOURCE_BRANCH \
-	$DOCKER_REPO:$SOURCE_BRANCH-$ARCH --os linux --arch $ARCH
-done
-
-docker manifest push --purge \
-	$DOCKER_REPO:$SOURCE_BRANCH
